5461c08caec6757496b0b5632ba28ad0f3e21738
ulfw <ulf.worsoe@mosek.com>
2016-10-06 13:26:32 +0200

    updating script

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+STATE=stable
+MAJORVER=7.1
+
+# Latest MOSEK distro version
+MOSEKVER=$(curl -s  http://download.mosek.com/$STATE/$MAJORVER/version)
+MSKSHORTVER=$(echo $MOSEKVER | sed 's/\([0-9]\+\.[0-9]\+\).[0-9]\+\.\([0-9]\+\)/\1.\2/g')
+MSKMAJMINVER=$(echo $MOSEKVER | sed 's/\([0-9]\+\.[0-9]\+\).[0-9]\+\.\([0-9]\+\)/\1/g')
+MSKREV=$(echo $MOSEKVER | sed 's/\([0-9]\+\.[0-9]\+\).[0-9]\+\.\([0-9]\+\)/\2/g')
+
+# Current version of PIP package
+PKGVER=$(grep '^Version:' PKG-INFO |sed 's/Version: \([0-9]\+\.[0-9]\+\)[ab.]\([0-9]\+\)/\1.\2/g')
+PKGREV=$(grep '^Version:' PKG-INFO |sed 's/Version: \([0-9]\+\.[0-9]\+\)[ab.]\([0-9]\+\)/\2/g')
+
+
+if (( $MSKREV > $PKGREV )); then
+    case "$STATE" in
+        alpha) NEWPKGVER="${MSKMAJMINVER}a$MSKREV" ;;
+        beta) NEWPKGVER="${MSKMAJMINVER}b$MSKREV" ;;
+        *) NEWPKGVER="${MSKMAJMINVER}.$MSKREV" ;;
+    esac
+
+    echo "Updating $PKGVER -> $NEWPKGVER"
+    mv PKG-INFO PKG-INFO.backup && \
+    sed "s/^Version: .*/Version: $NEWPKGVER/g" < PKG-INFO.backup > PKG-INFO && \
+    rm PKG-INFO.backup && \
+    git add PKG-INFO && \
+    git commit -m "update version to $NEWPKGVER" && \
+    git tag v"$NEWPKGVER" && \
+    git push origin "$NEWPKGVER" || exit 1
+else
+    echo Package version is already up-to-date
+fi
+
