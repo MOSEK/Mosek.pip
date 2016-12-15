31c9980c551f3ebd4ffb800a05dcb963150d2f40
ulfw <ulf.worsoe@mosek.com>
2016-12-15 09:49:20 +0100

    fix: setup.py regex

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class URLError(Exception): pass
 
 with open(os.path.join(os.path.dirname('__file__'),'PKG-INFO'),'rt') as f:
     data = f.read()
-    regex = re.compile(r'^Version:\s*([0-9]+)\.([0-9])+(?:\.([0-9]+))?(?:([ab])([0-9]+))',re.MULTILINE)
+    regex = re.compile(r'^Version:\s*([0-9]+)\.([0-9])+(?:\.([0-9]+))?(?:([ab])?([0-9]+))',re.MULTILINE)
     o = regex.search(data)
     if o is not None:
         mosekmajorver = o.group(1)
